1075e7452dd0609b5307f9d4127120b96824a67b
Billy0923 <Billy5279911@gmail.com>
2023-11-28 00:10:43 -0500

    Added basic route for companies and politicians

--- src/client/features/tasks/Tasks.jsx
+++ src/client/features/tasks/Tasks.jsx
@@ -1,7 +1,6 @@
 import { useSelector } from "react-redux";
 import { selectToken } from "../auth/authSlice";
-import NewTask from "./NewTask";
-import Task from "./Task";
+
 import { useGetTasksQuery } from "./taskSlice";
 
 import "./Tasks.less";
@@ -12,23 +11,12 @@ export default function Tasks() {
   const { data: tasks, isLoading } = useGetTasksQuery();
 
   if (!token) {
-    return <p>You must be logged in to see your tasks.</p>;
+    return <p>Please log in</p>;
   }
 
   return (
     <div className="tasks">
-      <h1>Tasks</h1>
-      <h2>Add New Task</h2>
-      <NewTask />
-      <h2>Your Tasks</h2>
-      {isLoading && <p>Loading tasks...</p>}
-      {tasks && (
-        <ul>
-          {tasks.map((task) => (
-            <Task key={task.id} task={task} />
-          ))}
-        </ul>
-      )}
+      This is home page
     </div>
   );
 }
