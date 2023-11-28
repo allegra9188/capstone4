8d7e96c65cce7be71b04549f73b7b8c03aed338b
Allegra McDermott <amcdermott9188@gmail.com>
2023-11-28 16:08:29 -0500

    articles portion made and working

--- src/client/main.jsx
+++ src/client/main.jsx
@@ -11,6 +11,8 @@ import Tasks from "./features/tasks/Tasks";
 import Politicians from "./features/politicians/Politicians.jsx";
 import Companies from "./features/companies/Companies.jsx";
 import Root from "./layout/Root.jsx";
+import ErrorPage from "./features/ErrorPage.jsx";
+import Articles from "./features/homepage/Articles.jsx";
 
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 const router = createBrowserRouter([
@@ -18,10 +20,11 @@ const router = createBrowserRouter([
     path: "/",
     element: <Root />,
     children: [
-      { path: "/", element: <Tasks /> },
+      { path: "/", element: <Articles /> },
       { path: "/politicians", element: <Politicians /> },
       { path: "/companies", element: <Companies /> },
       { path: "/login", element: <AuthForm /> },
+      { path: "*", element: <ErrorPage /> },
     ],
   },
 ]);
