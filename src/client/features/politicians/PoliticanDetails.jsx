68ece1e573575baab6ffe6a3237f29655e387897
Brian Capraro <Briancapraro0228@gmail.com>
2023-11-29 14:10:17 -0500

    Added politicianSlices.js and added get queries for all and single politicians.

--- src/client/features/politicians/PoliticanDetails.jsx
+++ src/client/features/politicians/PoliticanDetails.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { useParams } from 'react-router-dom'
+import { useGetPoliticianQuery } from './politicianSlice';
 
 function PoliticanDetails() {
     const { id } = useParams();
