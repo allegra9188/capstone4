4ba749b11537f0e31e4ad9b6b1c37778f754a421
Brian Capraro <Briancapraro0228@gmail.com>
2023-11-29 12:08:13 -0500

    Added route to get all politicians in politicians.js and added route to index.js.

--- src/server/api/politicians.js
+++ src/server/api/politicians.js
@@ -3,3 +3,13 @@ const prisma = require("../prisma");
 const router = require("express").Router();
 
 module.exports = router;
+
+// gets all politicians.
+router.get("/", async (req, res, next) => {
+  try {
+    const politicians = await prisma.Politician.findMany();
+    res.json(politicians);
+  } catch (err) {
+    next(err);
+  }
+});
