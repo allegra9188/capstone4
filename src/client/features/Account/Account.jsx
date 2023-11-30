e8d4c9d59daeb940775203c06c562d245f52c2fd
Allegra McDermott <amcdermott9188@gmail.com>
2023-11-30 11:48:57 -0500

    debugged errors

--- src/client/features/Account/Account.jsx
+++ src/client/features/Account/Account.jsx
@@ -2,34 +2,36 @@ import { useDispatch } from "react-redux";
 import { useNavigate, useParams } from "react-router-dom";
 import { logout } from "../auth/authSlice";
 import { useGetAccountQuery } from "../auth/authSlice";
-import "./account.css"
+import "./account.css";
 
-
-export default function Account(){
+export default function Account() {
   const { id } = useParams();
-  const { data: user, isLoading, isError} = useGetAccountQuery(id);
+  const { data: user, isLoading, isError } = useGetAccountQuery(id);
   const dispatch = useDispatch();
   const navigate = useNavigate();
-  
 
   const handleLogout = () => {
     dispatch(logout());
-    navigate('/')
+    navigate("/");
+  };
+
+  if (isLoading) {
+    return <div>Loading...</div>;
   }
 
-  if (isLoading) {return <div>Loading...</div>}
-  
-//   if (isError) {return <div>Error loading account</div>}
+  //   if (isError) {return <div>Error loading account</div>}
 
-//   console.log(user)
+  //   console.log(user)
 
-  return(
+  return (
     <div id="account-div">
-  <h1>Account: {user.email}</h1>
-  <h3>First Name: {user.firstName  + " " + user.lastName}</h3>
-  <h3>{user.id}</h3>
-  {/* <h1>Hello World</h1> */}
-  <button id="acct-logout" onClick={handleLogout}>Logout</button>
-  </div>
-  )
+      <h1>Account: {user.email}</h1>
+      <h3>First Name: {user.firstName + " " + user.lastName}</h3>
+      <h3>{user.id}</h3>
+      {/* <h1>Hello World</h1> */}
+      <button id="acct-logout" onClick={handleLogout}>
+        Logout
+      </button>
+    </div>
+  );
 }
