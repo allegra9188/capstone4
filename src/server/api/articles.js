36c7a8efb23497e859ebb0a0066bdbb08f56158a
Brian Capraro <Briancapraro0228@gmail.com>
2023-12-04 12:47:44 -0500

    Created articles.js in api on the backend.

---
+++ src/server/api/articles.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const router = express.Router();
+
+const url =
+  "https://api.marketaux.com/v1/news/all?countries=us&filter_entities=true&limit=10&published_after=2023-11-27T17:04&api_token=lVsq6J81NrdSum7Ba0fdlihd1rxpFzA1lRk5YwLT";
+
+router.get("/", async (req, res, next) => {
+  try {
+    const response = await fetch(url, {
+      method: "GET",
+    });
+    if (!response.ok) {
+      throw new Error(`HTTPS error! Status: ${response.status}`);
+    }
+    const data = await response.json();
+    console.log("Senate trading data", data);
+    res.json(data);
+  } catch (error) {
+    next(error);
+  }
+});
+
+module.exports = router;
