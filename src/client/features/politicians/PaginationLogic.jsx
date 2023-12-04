06dc638a15cbaa32ab2a3e5fb485a0566466c989
Allegra McDermott <amcdermott9188@gmail.com>
2023-12-04 11:47:26 -0500

    Search Regex working in Politicians page, and pagination added

---
+++ src/client/features/politicians/PaginationLogic.jsx
@@ -0,0 +1,36 @@
+import React, { useState } from "react";
+
+const PaginationLogic = ({ data, renderItem }) => {
+  const [currentPage, setCurrentPage] = useState(1);
+  const pageSize = 10;
+
+  const startIndex = (currentPage - 1) * pageSize;
+  const endIndex = startIndex + pageSize;
+
+  const paginatedData = data.slice(startIndex, endIndex);
+
+  const totalPages = Math.ceil(data.length / pageSize);
+
+  const handlePageChange = (newPage) => {
+    setCurrentPage(newPage);
+  };
+
+  return (
+    <>
+      <div>
+        {paginatedData.map((item) => renderItem(item))}
+      </div>
+      <div className="pagination">
+        <p>Page {currentPage} of {totalPages}</p>
+        <button disabled={currentPage === 1} onClick={() => handlePageChange(currentPage - 1)}>
+          Previous
+        </button>
+        <button disabled={currentPage === totalPages} onClick={() => handlePageChange(currentPage + 1)}>
+          Next
+        </button>
+      </div>
+    </>
+  );
+};
+
+export default PaginationLogic;
--- src/client/features/politicians/Politicians.jsx
+++ src/client/features/politicians/Politicians.jsx
@@ -1,23 +1,60 @@
-import React from "react";
-import PolitcianCard from "./PolitcianCard";
+import React, { useState } from "react";
+import PoliticianCard from "./PoliticianCard";
 import { useGetPoliticiansQuery } from "./politicianSlice";
+import PaginationLogic from "./PaginationLogic";
 
 export default function Politicians() {
-  // useGetPoliticiansQuery() is currently a placeholder
-  // will update once we get the slice, api, and endpoints up and running
-  // aware that below deconstructions is undefined and will cause error
   const { data: politicians, isLoading } = useGetPoliticiansQuery();
+  const [filter, setFilter] = useState("");
+  const [sortBy, setSortBy] = useState(""); // Added state for sorting
+
+  const searchRegex = new RegExp(filter, "i");
+
+  const handleSort = (e) => {
+    setSortBy(e.target.value);
+  };
+
+  const sortPoliticians = (politicians, sortBy) => {
+    if (sortBy === "first-name") {
+      return politicians.sort((a, b) => a.first_name.localeCompare(b.first_name));
+    } else if (sortBy === "last-name") {
+      return politicians.sort((a, b) => a.last_name.localeCompare(b.last_name));
+    } else {
+      return politicians; // No sorting
+    }
+  };
 
   return isLoading ? (
     <h2>Loading...</h2>
   ) : (
     <section>
-      <h1>Politicians</h1>
-      <div>
-        {[...politicians].map((politician) => (
-          <PolitcianCard key={politician.id} politician={politician} />
-        ))}
+      <div className="search-and-sort-container">
+        <form>
+          <input
+            type="text"
+            placeholder="Search..."
+            onChange={(e) => setFilter(e.target.value)}
+          />
+        </form>
+        <div>
+          <label>Sort By:</label>
+          <select id="sort-by" onChange={handleSort}>
+            <option value="">None</option>
+            <option value="first-name">First Name</option>
+            <option value="last-name">Last Name</option>
+          </select>
+        </div>
       </div>
+      <h1>Politicians</h1>
+      <PaginationLogic
+        data={sortPoliticians(politicians, sortBy).filter((politician) =>
+          (politician.first_name + politician.last_name).match(searchRegex)
+        )}
+        renderItem={(politician) => (
+          <PoliticianCard key={politician.id} politician={politician} />
+        )}
+      />
     </section>
   );
 }
+
