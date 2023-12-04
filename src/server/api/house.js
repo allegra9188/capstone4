b129d2ee8e2ec2b600fd17c8ec43d34a635a6d38
Brian Capraro <Briancapraro0228@gmail.com>
2023-12-04 12:27:07 -0500

    Revamped house.js in api on backend server, and added houseApiSlice.js

--- src/server/api/house.js
+++ src/server/api/house.js
@@ -1,18 +1,42 @@
-export default async function fetchHouseData() {
+// export default async function fetchHouseData() {
+//   try {
+//     const response = await fetch(
+//       "https://house-stock-watcher-data.s3-us-west-2.amazonaws.com/data/all_transactions.json"
+//     );
+
+//     if (!response.ok) {
+//       throw new Error(`Failed to fetch data. Status: ${response.status}`);
+//     }
+
+//     const data = await response.json();
+
+//     return data;
+//   } catch (error) {
+//     console.error("Error fetching data: ", error.message);
+//     throw error;
+//   }
+// }
+
+const express = require("express");
+const router = express.Router();
+
+const url =
+  "https://house-stock-watcher-data.s3-us-west-2.amazonaws.com/data/all_transactions.json";
+
+router.get("/", async (req, res, next) => {
   try {
-    const response = await fetch(
-      "https://house-stock-watcher-data.s3-us-west-2.amazonaws.com/data/all_transactions.json"
-    );
-
+    const response = await fetch(url, {
+      method: "GET",
+    });
     if (!response.ok) {
-      throw new Error(`Failed to fetch data. Status: ${response.status}`);
+      throw new Error(`HTTPS error! Status: ${response.status}`);
     }
-
     const data = await response.json();
-
-    return data;
+    console.log("House trading data", data);
+    res.json(data);
   } catch (error) {
-    console.error("Error fetching data: ", error.message);
-    throw error;
+    next(error);
   }
-}
+});
+
+module.exports = router;
