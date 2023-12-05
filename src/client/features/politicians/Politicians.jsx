7fd4be92e5e5ecf352efeb6cc18c8adb14740b9f
Brian Capraro <Briancapraro0228@gmail.com>
2023-12-05 16:27:35 -0500

    Filtered politicians works.

--- src/client/features/politicians/Politicians.jsx
+++ src/client/features/politicians/Politicians.jsx
@@ -3,13 +3,17 @@ import PoliticianCard from "./PoliticianCard";
 import { useGetPoliticiansQuery } from "./politicianSlice";
 import PaginationLogic from "./PaginationLogic";
 import "./PoliticianDetails.less";
-import checkActivity from "./TradeActivityChecker";
+import { checkActivity } from "./TradeActivityChecker";
+import { useGetSenateDataQuery } from "./senateApiSlice";
+import { useGetHouseDataQuery } from "./houseApiSlice";
 
 export default function Politicians() {
   const { data: politicians, isLoading } = useGetPoliticiansQuery();
   const [filter, setFilter] = useState("");
   const [sortBy, setSortBy] = useState("first-name"); // Set default sorting by first_name
   const [showActiveOnly, setShowActiveOnly] = useState(false);
+  const { data: senateTrades } = useGetSenateDataQuery();
+  const { data: houseTrades } = useGetHouseDataQuery();
 
   const searchRegex = new RegExp(filter, "i");
 
@@ -35,8 +39,12 @@ export default function Politicians() {
   };
 
   const filteredPoliticians = showActiveOnly
-    ? politicians.filter((politician) => checkActivity(politician))
+    ? politicians.filter((politician) =>
+        checkActivity(politician, houseTrades, senateTrades)
+      )
     : politicians;
+  console.log(filteredPoliticians);
+  console.log(politicians);
 
   return isLoading ? (
     <h2>Loading...</h2>
@@ -66,13 +74,14 @@ export default function Politicians() {
             checked={showActiveOnly}
             onChange={() => setShowActiveOnly(!showActiveOnly)}
           />
-          <label htmlFor="Active">Show Active Only</label>
+          <label for="Active">Show Active Only</label>
         </section>
       </div>
       <h1>Congress Politicians</h1>
       <PaginationLogic
-        data={sortPoliticians(politicians, sortBy).filter((politician) =>
-          (politician.first_name + politician.last_name).match(searchRegex)
+        data={sortPoliticians(filteredPoliticians, sortBy).filter(
+          (politician) =>
+            (politician.first_name + politician.last_name).match(searchRegex)
         )}
         renderItem={(politician) => (
           <PoliticianCard key={politician.id} politician={politician} />
--- src/client/features/politicians/TradeActivityChecker.jsx
+++ src/client/features/politicians/TradeActivityChecker.jsx
@@ -1,42 +1,41 @@
 import React from "react";
+export const checkActivity = (politician, houseTrades, senateTrades) => {
+  if (!politician) {
+    return false;
+  }
+  const fullName = `${politician.first_name} ${
+    politician.middle_name ? politician.middle_name + " " : ""
+  }${politician.last_name}`;
+
+  if (politician.role === "Rep" && Array.isArray(houseTrades)) {
+    // Check if there are any transactions for the representative
+    const representativeTrades = houseTrades.filter((transaction) => {
+      return (
+        transaction.representative &&
+        transaction.representative
+          .toLowerCase()
+          .includes(fullName.toLowerCase())
+      );
+    });
+
+    return representativeTrades.length > 0;
+  } else if (politician.role === "Sen" && Array.isArray(senateTrades)) {
+    // Check if there are any transactions for the senator
+    const senatorTrades = senateTrades.filter((transaction) => {
+      return (
+        transaction.senator &&
+        transaction.senator.toLowerCase().includes(fullName.toLowerCase())
+      );
+    });
+
+    return senatorTrades.length > 0;
+  }
+
+  return false; // Default to false if role is not recognized or trades are not arrays
+};
 
 const TradeActivityChecker = ({ politician, houseTrades, senateTrades }) => {
-  const checkActivity = (politician) => {
-    if (!politician) {
-      return false;
-    }
-    const fullName = `${politician.first_name} ${
-      politician.middle_name ? politician.middle_name + " " : ""
-    }${politician.last_name}`;
-
-    if (politician.role === "Rep" && Array.isArray(houseTrades)) {
-      // Check if there are any transactions for the representative
-      const representativeTrades = houseTrades.filter((transaction) => {
-        return (
-          transaction.representative &&
-          transaction.representative
-            .toLowerCase()
-            .includes(fullName.toLowerCase())
-        );
-      });
-
-      return representativeTrades.length > 0;
-    } else if (politician.role === "Sen" && Array.isArray(senateTrades)) {
-      // Check if there are any transactions for the senator
-      const senatorTrades = senateTrades.filter((transaction) => {
-        return (
-          transaction.senator &&
-          transaction.senator.toLowerCase().includes(fullName.toLowerCase())
-        );
-      });
-
-      return senatorTrades.length > 0;
-    }
-
-    return false; // Default to false if role is not recognized or trades are not arrays
-  };
-
-  const activityStatus = checkActivity(politician);
+  const activityStatus = checkActivity(politician, houseTrades, senateTrades);
   console.log(checkActivity(politician));
 
   return (
