3ec4395b98866d78621b17588037bb0249ae22f8
Brian Capraro <Briancapraro0228@gmail.com>
2023-12-14 12:01:15 -0500

    Added congress.jsx to politicians folder in features folder and updated path and navlink for page.

--- src/client/layout/Navbar.jsx
+++ src/client/layout/Navbar.jsx
@@ -21,44 +21,46 @@ export default function Navbar() {
   };
 
   // const [filter, setFilter] = useState("");
-  
 
   return (
     <>
-    <input type="checkbox" id="toggle"/>
-    <nav id="top">
-    <h1 id="title">Tradetivity</h1>
-      <label className="navbar-toggler" htmlFor="toggle">
-                <span className="bar"></span>
-                <span className="bar"></span>
-                <span className="bar"></span>
-            </label>
-      <menu className="nav-list">
-        <li className="nav-item">
-          <NavLink to="/">Home</NavLink>
-        </li>
-        <li className="nav-item">
-          <NavLink to="/politicians">Politicians</NavLink>
-        </li>
-        <li className="nav-item">
-          <NavLink to="/companies">Companies</NavLink>
-        </li>
-        {token ? (
-          <>
-            <li className="nav-item">
-              <NavLink to={`/user/${userid}`}>My Account</NavLink>
-            </li>
-            <li className="nav-item">
-              <a onClick={handleLogout}>Log Out</a>
-            </li>
-          </>
-        ) : (
+      <input type="checkbox" id="toggle" />
+      <nav id="top">
+        <h1 id="title">Tradetivity</h1>
+        <label className="navbar-toggler" htmlFor="toggle">
+          <span className="bar"></span>
+          <span className="bar"></span>
+          <span className="bar"></span>
+        </label>
+        <menu className="nav-list">
           <li className="nav-item">
-            <NavLink to="/login">Log In</NavLink>
+            <NavLink to="/">Home</NavLink>
           </li>
-        )}
-      </menu>
-    </nav>
+          <li className="nav-item">
+            <NavLink to="/politicians">Politicians</NavLink>
+          </li>
+          <li className="nav-item">
+            <NavLink to="/companies">Companies</NavLink>
+          </li>
+          <li className="nav-item">
+            <NavLink to="/congress">Congress</NavLink>
+          </li>
+          {token ? (
+            <>
+              <li className="nav-item">
+                <NavLink to={`/user/${userid}`}>My Account</NavLink>
+              </li>
+              <li className="nav-item">
+                <a onClick={handleLogout}>Log Out</a>
+              </li>
+            </>
+          ) : (
+            <li className="nav-item">
+              <NavLink to="/login">Log In</NavLink>
+            </li>
+          )}
+        </menu>
+      </nav>
     </>
   );
 }
