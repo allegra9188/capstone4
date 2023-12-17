5c58b913ca842f0de5ca39ed408afa678ca6d3b2
Allegra McDermott <amcdermott9188@gmail.com>
2023-12-17 14:40:05 -0500

    added pagination

--- src/client/features/Insights/Insights.jsx
+++ src/client/features/Insights/Insights.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { useGetInsightsQuery } from "./insightsSlice";
+import PaginationLogic from "../politicians/PaginationLogic";
 
 function Insights() {
   const { data, error, isLoading } = useGetInsightsQuery();
@@ -12,25 +13,29 @@ function Insights() {
     return <div>Error: {error.message}</div>;
   }
 
+  // Define the renderItem function for PaginationLogic
+  const renderItem = (item, index) => (
+    <div className="lobbying-card" key={index}>
+      <p>Latest Filing Date: {item.latest_filing_filing_date}</p>
+      <p><span id="bold">Specific Issues: </span>{item.specific_issues_0}</p>
+      <p><span id="bold">Lobbying Client: </span>{item.lobbying_client_name}</p>
+      <p>Lobbying Client Description: {item.lobbying_client_general_description}</p>
+      <p>Lobbying Registrant: {item.lobbying_registrant_name}</p>
+      <p>Lobbying Registrant Description: {item.lobbying_registrant_general_description}</p>
+      <p>Lobbyist Name: {item.lobbyists_0_name}</p>
+      <p>Lobbyist Position Covered: {item.lobbyists_0_covered_position}</p>
+      <p>In House: {item.inhouse}</p>
+      <p>Latest Filing URL: <a href={item.latest_filing_pdf_url}>{item.latest_filing_pdf_url}</a></p>
+    </div>
+  );
+
   return (
-    <div className="lobbying-container">
-      <h2>Live Lobbying</h2>
-      {data.map((entry, index) => (
-        <div className="lobbying-card" key={index}>
-          <p>Latest Filing Date: {entry.latest_filing_filing_date}</p>
-          <p><span id="bold">Specific Issues: </span>{entry.specific_issues_0}</p>
-          <p><span id="bold">Lobbying Client: </span>{entry.lobbying_client_name}</p>
-          <p>Lobbying Client Description: {entry.lobbying_client_general_description}</p>
-          <p>Lobbying Registrant: {entry.lobbying_registrant_name}</p>
-          <p>Lobbying Registrant Description: {entry.lobbying_registrant_general_description}</p>
-          <p>Lobbyist Name: {entry.lobbyists_0_name}</p>
-          <p>Lobbyist Position Covered: {entry.lobbyists_0_covered_position}</p>
-          <p>In House: {entry.inhouse}</p>
-          <p>Latest Filing URL: <a href={entry.latest_filing_pdf_url}>{entry.latest_filing_pdf_url}</a></p>
-          <p></p>
-        </div>
-      ))}
-      <h3>Sourced from ProPublica</h3>
+    <div>
+      <div className="lobbying-container">
+        <h2>Live Lobbying</h2>
+        <PaginationLogic data={data} pageSize={2} renderItem={renderItem} />
+        <h3>Sourced from ProPublica</h3>
+      </div>
     </div>
   );
 }
